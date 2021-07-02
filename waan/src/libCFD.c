54fc56a04350c86617ce59f659b94fa202341f61
Cristiano Fontana <cristiano.fontana@gmail.com>
2021-07-02 16:55:42 +0200

    Moved to realloc to reduce the number of allocations

--- waan/src/libCFD.c
+++ waan/src/libCFD.c
@@ -10,9 +10,6 @@
 #include "DSP_functions.h"
 
 /*! \brief Sctructure that holds the configuration for the `timestamp_analysis` function.
- *
- * This function parses a JSON object determining the configuration for the
- * `timestamp_analysis()` function.
  */
 struct CFD_config
 {
@@ -23,7 +20,20 @@ struct CFD_config
     uint32_t zero_crossing_samples;
     uint8_t fractional_bits;
     bool disable_shift;
+    bool disable_CFD_gates;
+
+    bool is_error;
+
+    uint32_t previous_samples_number;
+
+    double *curve_smoothed;
+    double *curve_centered;
+    double *curve_CFD;
 };
+
+/*! \brief Function that allocates the necessary memory for the calculations.
+ */
+void reallocate_curves(uint32_t samples_number, struct CFD_config **user_config);
 
 /*! \brief Function that reads the json_t configuration for the timestamp_analysis function.
  *
@@ -46,6 +56,9 @@ struct CFD_config
  *   Optional, default value: 10
  * - "disable_shift": disable the bit shift of the timestamp, in order to
  *   recalculate fine timestamps of previously analyzed data.
+ *   Optional, default value: false
+ * - "disable_CFD_gates": disable the display of the additional waveforms of
+ *   the CFD calculation.
  *   Optional, default value: false
  */
 void timestamp_init(json_t *json_config, void **user_config)
@@ -95,6 +108,19 @@ void timestamp_init(json_t *json_config, void **user_config)
             config->disable_shift = false;
         }
 
+        if (json_is_boolean(json_object_get(json_config, "disable_CFD_gates"))) {
+            config->disable_CFD_gates = json_is_true(json_object_get(json_config, "disable_CFD_gates"));
+        } else {
+            config->disable_CFD_gates = false;
+        }
+
+        config->is_error = false;
+        config->previous_samples_number = 0;
+
+        config->curve_smoothed = NULL;
+        config->curve_centered = NULL;
+        config->curve_CFD = NULL;
+
         (*user_config) = (void*)config;
     }
 }
@@ -103,6 +129,18 @@ void timestamp_init(json_t *json_config, void **user_config)
  */
 void timestamp_close(void *user_config)
 {
+    struct CFD_config *config = (struct CFD_config*)user_config;
+
+    if (config->curve_smoothed) {
+        free(config->curve_smoothed);
+    }
+    if (config->curve_centered) {
+        free(config->curve_centered);
+    }
+    if (config->curve_CFD) {
+        free(config->curve_CFD);
+    }
+
     if (user_config) {
         free(user_config);
     }
@@ -120,58 +158,32 @@ void timestamp_analysis(const uint16_t *samples,
 {
     struct CFD_config *config = (struct CFD_config*)user_config;
 
-    bool is_error = false;
+    reallocate_curves(samples_number, &config);
 
-    double *smoothed_curve = malloc(samples_number * sizeof(double));
-    double *centered_curve = malloc(samples_number * sizeof(double));
-    double *CFD_curve = malloc(samples_number * sizeof(double));
-
-    if (!smoothed_curve) {
-        printf("ERROR: libCFD timestamp_analysis(): Unable to allocate smoothed_curve memory\n");
-
-        is_error = true;
-    }
-    if (!centered_curve) {
-        printf("ERROR: libCFD timestamp_analysis(): Unable to allocate smoothed_curve memory\n");
-
-        is_error = true;
-    }
-    if (!CFD_curve) {
-        printf("ERROR: libCFD timestamp_analysis(): Unable to allocate CFD_curve memory\n");
-
-        is_error = true;
-    }
-
-    if (is_error) {
-        if (smoothed_curve) {
-            free(smoothed_curve);
-        }
-        if (centered_curve) {
-            free(centered_curve);
-        }
-        if (CFD_curve) {
-            free(CFD_curve);
-        }
+    if (config->is_error) {
+        printf("ERROR: libCFD timestamp_analysis(): Error status detected\n");
 
         return;
     }
 
-    running_mean(samples, samples_number, config->smooth_samples, &smoothed_curve);
+    running_mean(samples, samples_number, config->smooth_samples, &config->curve_smoothed);
 
     double baseline = 0;
 
-    calculate_average(smoothed_curve, 0, config->baseline_samples, &baseline);
+    calculate_average(config->curve_smoothed, 0, config->baseline_samples, &baseline);
 
-    add_constant(smoothed_curve, samples_number, -1 * baseline, &centered_curve);
+    add_and_multiply_constant(config->curve_smoothed, samples_number, \
+                              -1 * baseline, 1.0, \
+                              &config->curve_centered);
 
-    CFD_signal(centered_curve, samples_number, config->delay, config->fraction, &CFD_curve);
+    CFD_signal(config->curve_centered, samples_number, config->delay, config->fraction, &config->curve_CFD);
 
     double CFD_min = 0;
     double CFD_max = 0;
     size_t CFD_index_min = 0;
     size_t CFD_index_max = 0;
 
-    find_extrema(CFD_curve, 0, samples_number, &CFD_index_min, &CFD_index_max, &CFD_min, &CFD_max);
+    find_extrema(config->curve_CFD, 0, samples_number, &CFD_index_min, &CFD_index_max, &CFD_min, &CFD_max);
 
     size_t zero_crossing_index = 0;
 
@@ -184,12 +196,12 @@ void timestamp_analysis(const uint16_t *samples,
         CFD_index_right = CFD_index_min;
     }
 
-    find_zero_crossing(CFD_curve, CFD_index_left, CFD_index_right, &zero_crossing_index);
+    find_zero_crossing(config->curve_CFD, CFD_index_left, CFD_index_right, &zero_crossing_index);
 
     double fine_zero_crossing = 0;
 
     // The fine_zero_crossing contains also the zero_crossing_index information
-    find_fine_zero_crossing(CFD_curve, samples_number,
+    find_fine_zero_crossing(config->curve_CFD, samples_number,
                             zero_crossing_index, config->zero_crossing_samples,
                             &fine_zero_crossing);
 
@@ -214,54 +226,75 @@ void timestamp_analysis(const uint16_t *samples,
 
     (*trigger_position) = zero_crossing_index;
 
-    waveform_additional_set_number(waveform, 2);
+    if (!config->disable_CFD_gates) {
+        waveform_additional_set_number(waveform, 2);
 
-    unsigned int index = 0;
-    //uint8_t *additional_centered = waveform_additional_get(waveform, index);
-    //index++;
-    uint8_t *additional_CFD_signal = waveform_additional_get(waveform, index);
-    index++;
-    uint8_t *additional_trigger = waveform_additional_get(waveform, index);
+        unsigned int index = 0;
+        uint8_t *additional_CFD_signal = waveform_additional_get(waveform, index);
+        index++;
+        uint8_t *additional_trigger = waveform_additional_get(waveform, index);
 
-    //double centered_min = 0;
-    //double centered_max = 0;
-    //size_t centered_index_min = 0;
-    //size_t centered_index_max = 0;
-    //find_extrema(centered_curve, 0, samples_number, &centered_index_min, &centered_index_max, &centered_min, &centered_max);
+        const double CFD_abs_max = (fabs(CFD_max) > fabs(CFD_min)) ? fabs(CFD_max) : fabs(CFD_min);
 
-    //const double centered_abs_max = (fabs(centered_max) > fabs(centered_min)) ? fabs(centered_max) : fabs(centered_min);
-    const double CFD_abs_max = (fabs(CFD_max) > fabs(CFD_min)) ? fabs(CFD_max) : fabs(CFD_min);
+        const uint8_t ZERO = UINT8_MAX / 2;
+        const uint8_t MAX = UINT8_MAX / 2;
 
-    const uint8_t ZERO = UINT8_MAX / 2;
-    const uint8_t MAX = UINT8_MAX / 2;
+        for (uint32_t i = 0; i < samples_number; i++) {
+            additional_CFD_signal[i] = (config->curve_CFD[i] / CFD_abs_max) * MAX + ZERO;
 
-    for (uint32_t i = 0; i < samples_number; i++) {
-        //additional_centered[i] = (centered_curve[i] / centered_abs_max) * MAX + ZERO;
-        additional_CFD_signal[i] = (CFD_curve[i] / CFD_abs_max) * MAX + ZERO;
-
-        if (i == zero_crossing_index) {
-            additional_trigger[i] = ZERO + (MAX / 2);
-        } else if (i == zero_crossing_index + 1) {
-            additional_trigger[i] = ZERO - (MAX / 2);
-        } else if (i == CFD_index_max) {
-            additional_trigger[i] = ZERO + MAX;
-        } else if (i == CFD_index_min) {
-            additional_trigger[i] = ZERO - MAX;
-        } else {
-            additional_trigger[i] = ZERO;
+            if (i == zero_crossing_index) {
+                additional_trigger[i] = ZERO + (MAX / 2);
+            } else if (i == zero_crossing_index + 1) {
+                additional_trigger[i] = ZERO - (MAX / 2);
+            } else if (i == CFD_index_max) {
+                additional_trigger[i] = ZERO + MAX;
+            } else if (i == CFD_index_min) {
+                additional_trigger[i] = ZERO - MAX;
+            } else {
+                additional_trigger[i] = ZERO;
+            }
         }
     }
 
     (*select_event) = SELECT_TRUE;
+}
 
-    // Cleanup
-    if (smoothed_curve) {
-        free(smoothed_curve);
-    }
-    if (centered_curve) {
-        free(centered_curve);
-    }
-    if (CFD_curve) {
-        free(CFD_curve);
+void reallocate_curves(uint32_t samples_number, struct CFD_config **user_config)
+{
+    struct CFD_config *config = (*user_config);
+
+    if (samples_number != config->previous_samples_number) {
+	config->previous_samples_number = samples_number;
+
+        config->is_error = false;
+
+        double *new_curve_smoothed = realloc(config->curve_smoothed,
+                                             samples_number * sizeof(double));
+        double *new_curve_centered = realloc(config->curve_centered,
+                                             samples_number * sizeof(double));
+        double *new_curve_CFD = realloc(config->curve_CFD,
+                                        samples_number * sizeof(double));
+
+        if (!new_curve_smoothed) {
+            printf("ERROR: libCFD reallocate_curves(): Unable to allocate curve_smoothed memory\n");
+
+            config->is_error = true;
+        } else {
+            config->curve_smoothed = new_curve_smoothed;
+        }
+        if (!new_curve_centered) {
+            printf("ERROR: libCFD reallocate_curves(): Unable to allocate curve_centered memory\n");
+
+            config->is_error = true;
+        } else {
+            config->curve_centered = new_curve_centered;
+        }
+        if (!new_curve_CFD) {
+            printf("ERROR: libCFD reallocate_curves(): Unable to allocate curve_CFD memory\n");
+
+            config->is_error = true;
+        } else {
+            config->curve_CFD = new_curve_CFD;
+        }
     }
 }
