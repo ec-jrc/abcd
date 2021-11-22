c717142c6260352190bb5e0c28d7a175c91b6ef6
Cristiano Fontana <cristiano.fontana@gmail.com>
2021-11-22 10:58:12 +0100

    Added an energy threshold

--- waan/src/libCRRC4.c
+++ waan/src/libCRRC4.c
@@ -1,3 +1,39 @@
+/*! \brief Determination of the energy information from an exponentially
+ *         decaying pulse, by compensating its decay and then applying a CR-RC4
+ *         filter to the waveforms.
+ *
+ * Calculation procedure:
+ *  1. The baseline is determined averaging the first N samples.
+ *  2. The pulse is offset by the baseline to center it around zero.
+ *  3. The decay is compensated with a recursive filter, obtaining a step
+ *     function.
+ *  4. A recursive high-pass filter (CR filter) is applied.
+ *  5. A recursive low-pass filter (RC4 filter) is applied.
+ *  6. The energy information is obtained by determining the absolute maximum
+ *     of the resulting waveform.
+ *
+ * In the event_PSD structure the energy information is stored in the qlong,
+ * while the qshort stores the value of the absolute maximum of the waveform
+ * after the high-pass filter.
+ *
+ * The configuration parameters that are searched in a `json_t` object are:
+ *
+ * - `baseline_samples`: the number of samples to average to determine the
+ *   baseline. The average starts from the beginning of the waveform.
+ * - `pulse_polarity`: a string describing the expected pulse polarity, it
+ *   can be "positive` or "negative".
+ * - `decay_time`: the pulse decay time in terms of clock samples, for the
+ *   compensation.
+ * - `highpass_time`: the decay time of the high-pass filter (CR filter), in
+ *   terms of clock samples.
+ * - `lowpass_time`: the decay time of the low-pass filter (RC4 filter), in
+ *   terms of clock samples.
+ * - `height_scaling`: a scaling factor multiplied to both the integrals.
+ *   Optional, default value: 1
+ * - `energy_threshold`: pulses with an energy lower than the threshold are
+ *   discared. Optional, default value: 0
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -9,7 +45,7 @@
 #include "analysis_functions.h"
 #include "DSP_functions.h"
 
-/*! \brief Sctructure that holds the configuration for the `energy_analysis` function.
+/*! \brief Sctructure that holds the configuration for the `energy_analysis()` function.
  */
 struct CRRC4_config
 {
@@ -19,6 +55,7 @@ struct CRRC4_config
     double lowpass_time;
     enum pulse_polarity_t pulse_polarity;
     double height_scaling;
+    double energy_threshold;
 
     bool is_error;
 
@@ -35,26 +72,11 @@ struct CRRC4_config
  */
 void reallocate_curves(uint32_t samples_number, struct CRRC4_config **user_config);
 
-/*! \brief Function that reads the json_t configuration for the `energy_analysis` function.
+/*! \brief Function that reads the json_t configuration for the `energy_analysis()` function.
  *
  * This function parses a JSON object determining the configuration for the
  * `energy_analysis()` function. The configuration is returned as an
  * allocated `struct CRRC4_config`.
- *
- * The parameters that are searched in the json_t object are:
- *
- * - "baseline_samples": the number of samples to average to determine the
- *   baseline.
- * - "pulse_polarity": a string describing the expected pulse polarity, it
- *   can be "positive" or "negative".
- * - "decay_time": the pulse decay time in terms of clock samples, for the
- *   compensation.
- * - "highpass_time": the decay time of the high-pass filter, in terms of clock
- *   samples.
- * - "lowpass_time": the decay time of the low-pass filter, in terms of clock
- *   samples.
- * - "height_scaling": a scaling factor multiplied to both the integrals.
- *   Optional, default value: 1
  */
 void energy_init(json_t *json_config, void **user_config)
 {
@@ -65,7 +87,7 @@ void energy_init(json_t *json_config, void **user_config)
 
         (*user_config) = NULL;
     } else {
-	struct CRRC4_config *config = malloc(1 * sizeof(struct CRRC4_config));
+        struct CRRC4_config *config = malloc(1 * sizeof(struct CRRC4_config));
 
         if (!config) {
             printf("ERROR: libCRRC4 energy_init(): Unable to allocate config memory\n");
@@ -82,6 +104,12 @@ void energy_init(json_t *json_config, void **user_config)
             config->height_scaling = json_number_value(json_object_get(json_config, "height_scaling"));
         } else {
             config->height_scaling = 1;
+        }
+
+        if (json_is_number(json_object_get(json_config, "energy_threshold"))) {
+            config->energy_threshold = json_number_value(json_object_get(json_config, "energy_threshold"));
+        } else {
+            config->energy_threshold = 0;
         }
 
         config->pulse_polarity = POLARITY_NEGATIVE;
@@ -165,9 +193,12 @@ void energy_analysis(const uint16_t *samples,
 
     to_double(samples, samples_number, &config->curve_samples);
 
-    double baseline = 0;
+    // Preventing segfaults by checking the boundaries
+    const uint32_t baseline_start = 0;
+    const uint32_t baseline_end = (config->baseline_samples < samples_number) ? config->baseline_samples : samples_number;
 
-    calculate_average(config->curve_samples, 0, config->baseline_samples, &baseline);
+    double baseline = 0;
+    calculate_average(config->curve_samples, baseline_start, baseline_end, &baseline);
 
     if (config->pulse_polarity == POLARITY_POSITIVE) {
         add_and_multiply_constant(config->curve_samples, samples_number, -1 * baseline, 1.0, &config->curve_offset);
@@ -225,15 +256,15 @@ void energy_analysis(const uint16_t *samples,
                  &RC_index_min, &RC_index_max,
                  &RC_min, &RC_max);
 
-    const double energy_maximum = RC_max * config->height_scaling * config->lowpass_time / config->highpass_time;
-    const uint64_t long_maximum = (uint64_t)round(energy_maximum);
+    const double energy = RC_max * config->height_scaling * config->lowpass_time / config->highpass_time;
+    const uint64_t long_energy = (uint64_t)round(energy);
 
     // We convert the 64 bit integers to 16 bit to simulate the digitizer data
-    uint16_t int_maximum = long_maximum & UINT16_MAX;
+    uint16_t int_energy = long_energy & UINT16_MAX;
 
-    if (long_maximum > UINT16_MAX)
+    if (long_energy > UINT16_MAX)
     {
-        int_maximum = UINT16_MAX;
+        int_energy = UINT16_MAX;
     }
 
     uint64_t int_baseline = ((uint64_t)round(baseline)) & UINT16_MAX;
@@ -243,35 +274,39 @@ void energy_analysis(const uint16_t *samples,
     // Output
     event->timestamp = waveform->timestamp;
     event->qshort = int_CR_maximum;
-    event->qlong = int_maximum;
+    event->qlong = int_energy;
     event->baseline = int_baseline;
     event->channel = waveform->channel;
     event->pur = PUR;
 
-    const uint8_t initial_additional_number = waveform_additional_get_number(waveform);
-    const uint8_t new_additional_number = initial_additional_number + 3;
+    if (energy < config->energy_threshold) {
+        (*select_event) = SELECT_FALSE;
+    } else {
+        const uint8_t initial_additional_number = waveform_additional_get_number(waveform);
+        const uint8_t new_additional_number = initial_additional_number + 3;
 
-    waveform_additional_set_number(waveform, new_additional_number);
+        waveform_additional_set_number(waveform, new_additional_number);
 
-    uint8_t *additional_compensated = waveform_additional_get(waveform, initial_additional_number + 0);
-    uint8_t *additional_CR = waveform_additional_get(waveform, initial_additional_number + 1);
-    uint8_t *additional_RC = waveform_additional_get(waveform, initial_additional_number + 2);
+        uint8_t *additional_compensated = waveform_additional_get(waveform, initial_additional_number + 0);
+        uint8_t *additional_CR = waveform_additional_get(waveform, initial_additional_number + 1);
+        uint8_t *additional_RC = waveform_additional_get(waveform, initial_additional_number + 2);
 
-    const double compensated_abs_max = (fabs(compensated_max) > fabs(compensated_min)) ? fabs(compensated_max) : fabs(compensated_min);
-    const double CR_abs_max = (fabs(CR_max) > fabs(CR_min)) ? fabs(CR_max) : fabs(CR_min);
-    const double RC_abs_max = (fabs(RC_max) > fabs(RC_min)) ? fabs(RC_max) : fabs(RC_min);
-    //const double CRRC_abs_max = (RC_abs_max > CR_abs_max) ? RC_abs_max : CR_abs_max;
+        const double compensated_abs_max = (fabs(compensated_max) > fabs(compensated_min)) ? fabs(compensated_max) : fabs(compensated_min);
+        const double CR_abs_max = (fabs(CR_max) > fabs(CR_min)) ? fabs(CR_max) : fabs(CR_min);
+        const double RC_abs_max = (fabs(RC_max) > fabs(RC_min)) ? fabs(RC_max) : fabs(RC_min);
+        //const double CRRC_abs_max = (RC_abs_max > CR_abs_max) ? RC_abs_max : CR_abs_max;
 
-    const uint8_t ZERO = UINT8_MAX / 2;
-    const uint8_t MAX = UINT8_MAX / 2;
+        const uint8_t ZERO = UINT8_MAX / 2;
+        const uint8_t MAX = UINT8_MAX / 2;
 
-    for (uint32_t i = 0; i < samples_number; i++) {
-        additional_compensated[i] = (config->curve_compensated[i] / compensated_abs_max) * MAX + ZERO;
-        additional_CR[i] = (config->curve_CR[i] / CR_abs_max) * MAX + ZERO;
-        additional_RC[i] = (config->curve_RC[i] / RC_abs_max) * MAX + ZERO;
+        for (uint32_t i = 0; i < samples_number; i++) {
+            additional_compensated[i] = (config->curve_compensated[i] / compensated_abs_max) * MAX + ZERO;
+            additional_CR[i] = (config->curve_CR[i] / CR_abs_max) * MAX + ZERO;
+            additional_RC[i] = (config->curve_RC[i] / RC_abs_max) * MAX + ZERO;
+        }
+
+        (*select_event) = SELECT_TRUE;
     }
-
-    (*select_event) = SELECT_TRUE;
 }
 
 void reallocate_curves(uint32_t samples_number, struct CRRC4_config **user_config)
@@ -279,7 +314,7 @@ void reallocate_curves(uint32_t samples_number, struct CRRC4_config **user_confi
     struct CRRC4_config *config = (*user_config);
 
     if (samples_number != config->previous_samples_number) {
-	config->previous_samples_number = samples_number;
+        config->previous_samples_number = samples_number;
 
         config->is_error = false;
 
@@ -331,4 +366,3 @@ void reallocate_curves(uint32_t samples_number, struct CRRC4_config **user_confi
         }
     }
 }
-
