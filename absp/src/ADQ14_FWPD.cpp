ef6fa39e8865293c4848ff3800891310cfcf6716
Cristiano Fontana <cristiano.fontana@gmail.com>
2022-04-22 12:06:38 +0200

    Adding product name print

--- absp/src/ADQ14_FWPD.cpp
+++ absp/src/ADQ14_FWPD.cpp
@@ -139,7 +139,7 @@ int ABCD::ADQ14_FWPD::Initialize(void* adq, int num)
         std::cout << std::endl;
         std::cout << '[' << time_buffer << "] ABCD::ADQ14_FWPD::Initialize() ";
         std::cout << "Card name (serial number): " << GetName() << "; ";
-        std::cout << "Product name: " << ADQ_GetBoardProductName(adq_cu_ptr, adq_num) << "; ";
+        std::cout << "Product name: " << ADQ_GetBoardProductName(adq_cu_ptr, adq14_num) << "; ";
         std::cout << "USB address: " << ADQ_GetUSBAddress(adq_cu_ptr, adq14_num) << "; ";
         std::cout << "PCIe address: " << ADQ_GetPCIeAddress(adq_cu_ptr, adq14_num) << "; ";
         std::cout << std::endl;
