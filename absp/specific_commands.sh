c8bdd237407d8c13adddc66d07ffa901a8759dec
Cristiano Fontana <cristiano.fontana@gmail.com>
2022-09-28 17:07:41 +0200

    Added an example of specific commands for ADQ214 and ADQ412

---
+++ absp/specific_commands.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# Examples of specific commands for the ADQ214
+
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "timestamp_reset", "mode": "pulse"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-03867", "command": "timestamp_reset", "mode": "pulse"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-02020", "command": "timestamp_reset", "mode": "pulse"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01335", "command": "timestamp_reset", "mode": "pulse"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-03875", "command": "timestamp_reset", "mode": "pulse"}'
+#
+#sleep 2
+#
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_set_direction", "direction": "output"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_pulse", "width": 0}'
+#
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_write", "value": 1}'
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_read"}'
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_write", "value": 0}'
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_read"}'
+
+# Examples of specific commands for the ADQ412
+
+~/abcd/bin/send_command.py specific -a '{"serial": "SPD-00858", "command": "timestamp_reset", "mode": "pulse"}'
+~/abcd/bin/send_command.py specific -a '{"serial": "SPD-00864", "command": "timestamp_reset", "mode": "pulse"}'
+
+#sleep 2
+#
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_set_direction", "direction": "output"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_pulse", "width": 0}'
+#
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_write", "value": 1}'
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_read"}'
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_write", "value": 0}'
+#../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_read"}'
