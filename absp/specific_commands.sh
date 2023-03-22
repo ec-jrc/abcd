d53156ecb805e8eab6f0b0b9c1ca3fe740817e46
Cristiano Fontana <cristiano.fontana@gmail.com>
2023-03-22 15:43:07 +0100

    Added examples for the ADQ14

--- absp/specific_commands.sh
+++ absp/specific_commands.sh
@@ -20,9 +20,9 @@
 
 # Examples of specific commands for the ADQ412
 
-~/abcd/bin/send_command.py specific -a '{"serial": "SPD-00858", "command": "timestamp_reset", "mode": "pulse"}'
-~/abcd/bin/send_command.py specific -a '{"serial": "SPD-00864", "command": "timestamp_reset", "mode": "pulse"}'
-
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-00858", "command": "timestamp_reset", "mode": "pulse"}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-00864", "command": "timestamp_reset", "mode": "pulse"}'
+#
 #sleep 2
 #
 #~/abcd/bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_set_direction", "direction": "output"}'
@@ -32,3 +32,9 @@
 #../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_read"}'
 #../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_write", "value": 0}'
 #../bin/send_command.py specific -a '{"serial": "SPD-01373", "command": "GPIO_read"}'
+
+# Examples of specific commands for the ADQ14
+
+# Using the trig port as an output
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-07721", "command": "GPIO_set_direction", "port": 2, "direction": 1, "mask": 0}'
+#~/abcd/bin/send_command.py specific -a '{"serial": "SPD-07721", "command": "GPIO_pulse", "port": 2, "mask": 0, "width": 0}'
