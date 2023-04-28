d5a28c449cf5d9c16972646278e6be8f2dfddf99
Cristiano Fontana <cristiano.fontana@gmail.com>
2023-04-28 10:14:43 +0200

    Selecting multiple channels and not only one

--- bin/plot_timestamps.py
+++ bin/plot_timestamps.py
@@ -19,6 +19,7 @@
 
 import argparse
 import os
+import traceback
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,13 +35,13 @@ def exponential(t, A, tau):
     return A * np.exp(-t / tau)
 
 parser = argparse.ArgumentParser(description='Plots multiple timestamps sequences and distributions of time differences from ABCD events data files.')
-parser.add_argument('file_names',
+parser.add_argument('file_name',
                     type = str,
+                    help = 'File name')
+parser.add_argument('channels',
+                    type = int,
                     nargs = '+',
-                    help = 'List of space-separated file names')
-parser.add_argument('channel',
-                    type = str,
-                    help = 'Channel selection (all or number)')
+                    help = 'List of space-separated channels to select')
 parser.add_argument('-n',
                     '--ns_per_sample',
                     type = float,
@@ -55,7 +56,7 @@ parser.add_argument('-d',
                     '--delta_min',
                     type = float,
                     default = DELTA_MIN,
-                    help = 'Minimum time difference (default: {:f})'.format(DELTA_MIN))
+                    help = 'Minimum time difference, in seconds or timestamp unit (default: {:f})'.format(DELTA_MIN))
 parser.add_argument('-D',
                     '--delta_max',
                     type = float,
@@ -70,6 +71,9 @@ parser.add_argument('-s',
                     '--save_data',
                     action = "store_true",
                     help = 'Save histograms to file')
+parser.add_argument('--relative_indexes',
+                    action = "store_true",
+                    help = 'Instead of plotting vs the aboslute indexes, use the consecutive indexes of the timestamps')
 
 args = parser.parse_args()
 
@@ -88,74 +92,86 @@ event_PSD_dtype = np.dtype([('timestamp', np.uint64),
                             ('pur', np.uint8),
                             ])
 
-timestamps = dict()
+buffer_size = args.buffer_size - (args.buffer_size % 16)
+print("Using buffer size: {:d}".format(buffer_size))
+
+file_name = args.file_name
+print("Reading: {}".format(file_name))
+
+indexes_timestamps = {channel: list() for channel in args.channels}
+partial_timestamps = {channel: list() for channel in args.channels}
+
+index_previous = 0
+
+min_times = {channel: list() for channel in args.channels}
+max_times = {channel: list() for channel in args.channels}
+events_counter = {channel: 0 for channel in args.channels}
+chunks_counter = 0
+
+# We will read the file in chunks so we can process also very big files
+with open(file_name, "rb") as input_file:
+    while True:
+        try:
+            print("    Reading chunk: {:d}".format(chunks_counter))
+
+            file_chunk = input_file.read(buffer_size)
+
+            print("    Chunk length: {:d}".format(len(file_chunk)))
+
+            if len(file_chunk) <= 0:
+                break
+
+            data = np.frombuffer(file_chunk, dtype = event_PSD_dtype)
+
+            indexes = np.arange(index_previous, index_previous + len(data))
+
+            this_timestamps = data['timestamp']
+
+            for channel in args.channels:
+                channel_selection = data['channel'] == channel
+
+                events_counter[channel] += channel_selection.sum()
+
+                indexes_timestamps[channel].extend(indexes[channel_selection])
+                partial_timestamps[channel].extend(this_timestamps[channel_selection])
+
+                min_times[channel].append(min(partial_timestamps[channel]))
+                max_times[channel].append(max(partial_timestamps[channel]))
+
+
+            index_previous += len(data)
+
+            chunks_counter += 1
+
+        except Exception as error:
+            print("    ERROR: {}".format(error))
+            break
+
 time_deltas_histos = dict()
 time_deltas_edges = dict()
 As = dict()
 taus = dict()
 
-buffer_size = args.buffer_size - (args.buffer_size % 16)
-print("Using buffer size: {:d}".format(buffer_size))
+for channel in args.channels:
+    min_time = min(min_times[channel])
+    max_time = max(max_times[channel])
 
-for file_name in args.file_names:
-    print("Reading: {}".format(file_name))
+    Delta_time = (max_time - min_time) * conversion_factor
 
-    partial_timestamps = list()
-    min_times = list()
-    max_times = list()
-    counter = 0
-    events_counter = 0
+    rate_measured = events_counter[channel] / Delta_time
 
-    # We will read the file in chunks so we can process also very big files
-    with open(file_name, "rb") as input_file:
-        while True:
-            try:
-                print("    Reading chunk: {:d}".format(counter))
-
-                file_chunk = input_file.read(buffer_size)
-
-                data = np.frombuffer(file_chunk, dtype = event_PSD_dtype)
-
-                this_timestamps = data['timestamp']
-
-                min_times.append(min(this_timestamps))
-                max_times.append(max(this_timestamps))
-
-                try:
-                    channel = int(args.channel)
-                    channel_selection = data['channel'] == channel
-                except ValueError:
-                    channel_selection = data['channel'] >= 0
-
-                events_counter += channel_selection.sum()
-
-                partial_timestamps.extend(this_timestamps[channel_selection])
-
-                counter += 1
-
-            except Exception as error:
-                print("    ERROR: {}".format(error))
-                break
+    print("Channel: {:d}".format(channel))
+    print("    Number of events: {:d}".format(events_counter[channel]))
+    print("    Time interval: [{:g}, {:g}] {}".format(min_time * conversion_factor,
+                                                      max_time * conversion_factor, unit))
+    print("    Time delta: {:g} {}".format(Delta_time, unit))
+    print("    Measured rate: {:g} events/{}".format(rate_measured, unit))
 
     try:
-        min_time = min(min_times) * conversion_factor
-        max_time = max(max_times) * conversion_factor
+        timestamps_left = np.asarray(partial_timestamps[channel][:-1])
+        timestamps_right = np.asarray(partial_timestamps[channel][1:])
 
-        Delta_time = max_time - min_time
-
-        rate_measured = events_counter / Delta_time
-
-        print("    Number of events: {:d}".format(events_counter))
-        print("    Time interval: [{:g}, {:g}] {}".format(min_time, max_time, unit))
-        print("    Time delta: {:g} {}".format(Delta_time, unit))
-        print("    Measured rate: {:g} 1/{}".format(rate_measured, unit))
-
-        timestamps[file_name] = partial_timestamps
-
-        timestamps_left = np.asarray(partial_timestamps[:-1]) * conversion_factor
-        timestamps_right = np.asarray(partial_timestamps[1:]) * conversion_factor
-
-        time_deltas = timestamps_right - timestamps_left
+        time_deltas = (timestamps_right - timestamps_left) * conversion_factor
 
         if args.delta_max is None:
             delta_max = max(time_deltas)
@@ -163,11 +179,11 @@ for file_name in args.file_names:
             delta_max = args.delta_max
 
         histo, edges = np.histogram(time_deltas,
-                                    bins = args.delta_bins,
-                                    range = (args.delta_min, delta_max))
+                                   bins = args.delta_bins,
+                                   range = (args.delta_min, delta_max))
 
-        time_deltas_histos[file_name] = histo
-        time_deltas_edges[file_name] = edges
+        time_deltas_histos[channel] = histo
+        time_deltas_edges[channel] = edges
 
         A = histo.max()
         I = np.trapz(histo, x = edges[:-1])
@@ -177,42 +193,51 @@ for file_name in args.file_names:
 
         popt, pcov = optimize.curve_fit(exponential, edges[:-1], histo, p0=p0)
 
-        As[file_name] = popt[0]
-        taus[file_name] = popt[1]
-        
-        rate_true = 1. / taus[file_name]
+        As[channel] = popt[0]
+        taus[channel] = popt[1]
 
-        dead_time = (rate_true - rate_measured) / (rate_true * rate_measured)
+        rate_true = 1. / taus[channel]
 
-        print("    True rate: {:g} 1/{}".format(rate_true, unit))
+        dead_time = Delta_time - (events_counter[channel] / rate_true)
+
+        print("    True rate: {:g} events/{}".format(rate_true, unit))
         print("    Dead time: {:g} {} ({:f}%)".format(dead_time, unit, dead_time / Delta_time * 100))
 
     except Exception as error:
         print("    ERROR: {}".format(error))
+        print(traceback.format_exc())
 
 if args.save_data:
-    for file_name in args.file_names:
-        try:
-            basename, extension = os.path.splitext(file_name)
+    try:
+        basename, extension = os.path.splitext(file_name)
 
-            output_file_name = basename + '_ch{}_timedelta.csv'.format(channel)
-            output_array = np.vstack((time_deltas_edges[file_name][:-1], time_deltas_histos[file_name])).T
+        output_file_name = basename + '_ch{}_timedelta.csv'.format(channel)
+        output_array = np.vstack((time_deltas_edges[:-1], time_deltas_histos)).T
 
-            print("    Writing qlong histogram to: {}".format(output_file_name))
-            np.savetxt(output_file_name, output_array, delimiter = ',', header = 'energy,counts')
-        except Exception as error:
-            print("    ERROR: {}".format(error))
+        print("    Writing qlong histogram to: {}".format(output_file_name))
+        np.savetxt(output_file_name, output_array, delimiter = ',', header = 'energy,counts')
+    except Exception as error:
+        print("    ERROR: {}".format(error))
+        print(traceback.format_exc())
 else:
     fig = plt.figure()
 
     ts_ax = fig.add_subplot(111)
 
-    for file_name in args.file_names:
+    for channel in args.channels:
         try:
-            ts_ax.plot(np.asarray(timestamps[file_name]) * conversion_factor,
-                       label = file_name)
+            if args.relative_indexes:
+                ts_ax.plot(np.asarray(partial_timestamps[channel]) * conversion_factor,
+                           linewidth = 0, marker = ',',
+                           label = "Ch: {:d}".format(channel))
+            else:
+                ts_ax.plot(indexes_timestamps[channel],
+                           np.asarray(partial_timestamps[channel]) * conversion_factor,
+                           linewidth = 0, marker = ',',
+                           label = "Ch: {:d}".format(channel))
         except Exception as error:
             print("    ERROR: {}".format(error))
+            print(traceback.format_exc())
 
     ts_ax.set_ylabel('Timestamps [{}]'.format(unit))
     ts_ax.set_xlabel('Timestamp index')
@@ -223,17 +248,18 @@ else:
 
     td_ax = fig.add_subplot(111)
 
-    for file_name in args.file_names:
+    for channel in args.channels:
         try:
-            td_ax.step(time_deltas_edges[file_name][:-1],
-                       time_deltas_histos[file_name],
-                       where = 'post', label = file_name)
+            td_ax.step(time_deltas_edges[channel][:-1],
+                       time_deltas_histos[channel],
+                       where = 'post', label = "Ch: {:d}".format(channel))
 
-            td_ax.plot(time_deltas_edges[file_name][:-1],
-                       exponential(time_deltas_edges[file_name][:-1], As[file_name], taus[file_name]),
+            td_ax.plot(time_deltas_edges[channel][:-1],
+                       exponential(time_deltas_edges[channel][:-1], As[channel], taus[channel]),
                        label = 'Fit')
         except Exception as error:
             print("    ERROR: {}".format(error))
+            print(traceback.format_exc())
 
     td_ax.set_xlabel('$\delta t = t_{n} - t_{n-1}$' + ' [{}]'.format(unit))
     td_ax.grid()
