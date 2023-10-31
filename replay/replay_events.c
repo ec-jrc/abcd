8965f714db0bbdb7faf01164d56934aeab4276da
Cristiano Fontana <cristiano.fontana@gmail.com>
2023-10-31 10:52:22 +0100

    Added continuous execution

--- replay/replay_events.c
+++ replay/replay_events.c
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2016 Cristiano Lino Fontana
+ * (C) Copyright 2016, 2023, European Union, Cristiano Lino Fontana
  *
  * This file is part of ABCD.
  *
@@ -34,11 +34,28 @@
 #include <stdlib.h>
 // For memcpy
 #include <string.h>
+#include <signal.h>
 
 #include <zmq.h>
 
 #include "defaults.h"
 #include "socket_functions.h"
+
+bool terminate_flag = false;
+
+//! Handles standard signals.
+/*! SIGTERM (from kill): terminates kindly closing the input file.
+    SIGINT (from ctrl-c): same behaviour as SIGTERM
+    SIGHUP (from shell processes): same behaviour as SIGTERM
+ */
+void signal_handler(int signum)
+{
+    if (signum == SIGINT || signum == SIGTERM || signum == SIGHUP)
+    {
+        terminate_flag = true;
+    }
+}
+
 
 void print_usage(const char *name) {
     printf("Usage: %s [options] <file_name>\n", name);
@@ -49,6 +66,7 @@ void print_usage(const char *name) {
     printf("\t-h: Display this message\n");
     printf("\t-v: Set verbose execution\n");
     printf("\t-V: Set verbose execution with more details\n");
+    printf("\t-c: Enable continuous execution\n");
     printf("\t-D <address>: Data socket address, default: %s\n", defaults_abcd_data_address);
     printf("\t-T <period>: Set base period in milliseconds, default: %d\n", defaults_replay_base_period);
     printf("\t             For a very fast replay, 0 ms is also accepted.\n");
@@ -65,9 +83,10 @@ int main(int argc, char *argv[])
     char *data_address = defaults_abcd_data_address;
     unsigned int skip_packets = defaults_replay_skip;
     size_t buffer_size = defaults_all_topic_buffer_size;
+    bool continuous_execution = false;
 
     int c = 0;
-    while ((c = getopt(argc, argv, "hD:T:B:vVs:")) != -1) {
+    while ((c = getopt(argc, argv, "hD:T:B:vVs:c")) != -1) {
         switch (c) {
             case 'h':
                 print_usage(argv[0]);
@@ -83,6 +102,9 @@ int main(int argc, char *argv[])
                 break;
             case 's':
                 skip_packets = atoi(optarg);
+                break;
+            case 'c':
+                continuous_execution = true;
                 break;
             case 'v':
                 verbosity = 1;
@@ -108,6 +130,7 @@ int main(int argc, char *argv[])
         printf("Data socket address: %s\n", data_address);
         printf("Buffer size: %zu\n", buffer_size);
         printf("File: %s\n", file_name);
+        printf("Continuous execution: %s\n", continuous_execution ? "true" : "false");
         printf("Verbosity: %u\n", verbosity);
         printf("Base period: %u\n", base_period);
         printf("Packets to be skipped: %u\n", skip_packets);
@@ -149,93 +172,109 @@ int main(int argc, char *argv[])
     wait.tv_sec = base_period / 1000;
     wait.tv_nsec = (base_period % 1000) * 1000000L;
 
-    FILE *in_file = fopen(file_name, "rb");
+    size_t loop_counter = 0;
 
-    if (!in_file)
-    {
-        printf("ERROR: Unable to open: %s\n", file_name);
-    }
-    else
-    {
-        // Compute the data topic
-        char data_topic[defaults_all_topic_buffer_size];
-        memset(data_topic, '\0', defaults_all_topic_buffer_size);
-
-        // I am not sure if snprintf is standard or not, apprently it is in the C99 standard
-        snprintf(data_topic, defaults_all_topic_buffer_size, "data_abcd_events_v0_s%zu", 16 * buffer_size);
-
-        void *buffer = malloc(16 * buffer_size);
-
-        if (buffer == NULL)
+    while (terminate_flag == false) {
+        if ((verbosity > 0) && (continuous_execution == true))
         {
-            printf("ERROR: Unable to allocate buffer\n");
+            printf("Loop number: %zu\n", loop_counter);
+        }
+
+        FILE *in_file = fopen(file_name, "rb");
+
+        if (!in_file)
+        {
+            printf("ERROR: Unable to open: %s\n", file_name);
         }
         else
         {
-            size_t bytes_counter = 0;
-            size_t msg_id = 0;
+            // Compute the data topic
+            char data_topic[defaults_all_topic_buffer_size];
+            memset(data_topic, '\0', defaults_all_topic_buffer_size);
 
-            while ((!ferror(in_file)) && (!feof(in_file)))
+            // I am not sure if snprintf is standard or not, apprently it is in the C99 standard
+            snprintf(data_topic, defaults_all_topic_buffer_size, "data_abcd_events_v0_s%zu", 16 * buffer_size);
+
+            void *buffer = malloc(16 * buffer_size);
+
+            if (buffer == NULL)
             {
-                const size_t bytes_read = fread(buffer, 16, buffer_size, in_file);
+                printf("ERROR: Unable to allocate buffer\n");
+            }
+            else
+            {
+                size_t bytes_counter = 0;
+                size_t msg_id = 0;
 
-                bytes_counter += bytes_read * 16;
-
-                if (verbosity > 1)
+                while ((!ferror(in_file)) && (!feof(in_file)))
                 {
-                    printf("read: %zu, requested: %zu\n", bytes_read, buffer_size);
+                    const size_t bytes_read = fread(buffer, 16, buffer_size, in_file);
 
-                    if (bytes_read < buffer_size)
+                    bytes_counter += bytes_read * 16;
+
+                    if (verbosity > 1)
                     {
-                        printf("WARNING: Read bytes are less than requested\n");
+                        printf("read: %zu, requested: %zu\n", bytes_read, buffer_size);
+
+                        if (bytes_read < buffer_size)
+                        {
+                            printf("WARNING: Read bytes are less than requested\n");
+                        }
                     }
-                }
 
-                if (verbosity > 0)
-                {
-                    printf("Topic [%zu]: %s\n", msg_id, data_topic);
-                }
-
-                if (msg_id < skip_packets)
-                {
                     if (verbosity > 0)
                     {
-                        printf("INFO: Skipping packet\n");
+                        printf("Topic [%zu]: %s\n", msg_id, data_topic);
                     }
-                }
-                else
-                {
-                    send_byte_message(data_socket, data_topic, buffer, bytes_read * 16, verbosity);
+
+                    if (msg_id < skip_packets)
+                    {
+                        if (verbosity > 0)
+                        {
+                            printf("INFO: Skipping packet\n");
+                        }
+                    }
+                    else
+                    {
+                        send_byte_message(data_socket, data_topic, buffer, bytes_read * 16, verbosity);
+                    }
+
+                    if (verbosity > 0)
+                    {
+                        printf("Read size: %zu B\n", bytes_counter);
+                    }
+
+                    if (msg_id >= skip_packets)
+                    {
+                        // Putting a delay in order not to fill-up the queues
+                        nanosleep(&wait, NULL);
+                    }
+
+                    msg_id += 1;
                 }
 
-                if (verbosity > 0)
+                if (ferror(in_file))
                 {
-                    printf("Read size: %zu B\n", bytes_counter);
+                    printf("ERROR: Reading error\n");
                 }
 
-                if (msg_id >= skip_packets)
+                if (feof(in_file) && (verbosity > 0))
                 {
-                    // Putting a delay in order not to fill-up the queues
-                    nanosleep(&wait, NULL);
+                    printf("End of file\n");
                 }
 
-                msg_id += 1;
+                free(buffer);
             }
 
-            if (ferror(in_file))
-            {
-                printf("ERROR: Reading error\n");
-            }
-
-            if (feof(in_file) && (verbosity > 0))
-            {
-                printf("End of file\n");
-            }
-
-            free(buffer);
+            fclose(in_file);
         }
 
-        fclose(in_file);
+        loop_counter += 1;
+
+        if (continuous_execution == false) {
+            terminate_flag = true;
+        }
+
     }
 
     // Wait a bit to allow the sockets to deliver
