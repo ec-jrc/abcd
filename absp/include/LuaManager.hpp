7d40fcad747b16bdc9d30932cbc95cb6f5d461c1
Cristiano Fontana <cristiano.fontana@gmail.com>
2026-06-19 16:28:07 +0200

    Ported to spdlog

--- absp/include/LuaManager.hpp
+++ absp/include/LuaManager.hpp
@@ -1,12 +1,14 @@
 #ifndef LUAMANAGER_HPP
 #define LUAMANAGER_HPP
 
-#include <iostream>
 // For std::this_thread::sleep_for
 #include <thread>
 #include <chrono>
 
 #include <lua.hpp>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 #include "ADQ_descriptions.hpp"
 #include "ADQ_utilities.hpp"
@@ -28,9 +30,11 @@ extern int luaopen_ADQAPI(lua_State* L);
 extern int luaopen_digitizers(lua_State* L);
 }
 
+extern std::shared_ptr<spdlog::logger> absp_logger_console;
+extern std::shared_ptr<spdlog::logger> absp_logger_error;
+
 class LuaManager {
 private:
-    int verbosity;
     lua_State* Lua; // the Lua state
 
     unsigned int counter_updates;
@@ -45,15 +49,20 @@ private:
     swig_type_info *SWIG_ABCD_ADQ36_FWDAQ_t;
 
 public:
-    LuaManager(int V = 0) : verbosity(V), counter_updates(0), counter_runs(0) {
+    LuaManager() : counter_updates(0), counter_runs(0) {
+        if (absp_logger_console == nullptr) {
+            absp_logger_console = spdlog::stdout_color_mt("lua");
+
+            absp_logger_console->set_level(spdlog::level::info);
+        }
+        if (absp_logger_error == nullptr) {
+            absp_logger_error = spdlog::stderr_color_mt("lua_error");
+        }
+
         // Create a new state
         Lua = luaL_newstate();
 
-        if (verbosity > 1) {
-            std::cout << "LuaManager::LuaManager() ";
-            std::cout << "Stack length: " << lua_gettop(Lua) << "; ";
-            std::cout << std::endl;
-        }
+        absp_logger_console->debug("Stack length: {}", lua_gettop(Lua));
 
         // Load the standard libraries
         luaL_openlibs(Lua);
@@ -65,6 +74,9 @@ public:
 
         lua_pushcfunction(Lua, LuaManager::sleep);
         lua_setglobal(Lua, "sleep");
+
+        lua_pushcfunction(Lua, LuaManager::abcd_log);
+        lua_setglobal(Lua, "abcd_log");
 
         // Create a new table in which to store the digitizers
         lua_newtable(Lua);
@@ -84,9 +96,7 @@ public:
         // I put these here, otherwise they give a segfault if put in the
         // update_digitizers() method
         // FIXME: Understand why is it the case...
-        if (verbosity > 1) {
-            std::cout << "LuaManager::LuaManager() Type query; " << std::endl;
-        }
+        absp_logger_console->debug("LuaManager::LuaManager() Type query");
 
         SWIG_ABCD_Digitizer_t = SWIG_TypeQuery(Lua, "ABCD::Digitizer *");
         SWIG_ABCD_ADQ214_t = SWIG_TypeQuery(Lua, "ABCD::ADQ214 *");
@@ -95,55 +105,50 @@ public:
         SWIG_ABCD_ADQ14_FWPD_t = SWIG_TypeQuery(Lua, "ABCD::ADQ14_FWPD *");
         SWIG_ABCD_ADQ36_FWDAQ_t = SWIG_TypeQuery(Lua, "ABCD::ADQ36_FWDAQ *");
 
-        if (verbosity > 1) {
-            run_string("print(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::LuaManager() Finished\");");
+        if (absp_logger_console->should_log(spdlog::level::debug))
+        {
+            run_string("abcd_log(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::LuaManager() Finished\");");
 
             dump_stack();
         }
     }
 
     ~LuaManager() {
-        if (verbosity > 0) {
-            run_string("print(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::~LuaManager()\");");
+        if (absp_logger_console->should_log(spdlog::level::info))
+        {
+            run_string("abcd_log(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::~LuaManager()\");");
         }
 
         // Close the lua state
         lua_close(Lua);
     }
 
-    void set_verbosity(int v) {
-        verbosity = v;
-    }
-
     // A helper function that prints the stack length and content
     void dump_stack() {
-        // Get the stack length
         const int top = lua_gettop(Lua);
 
-        std::cout << "LuaManager::dump_stack() ";
-        std::cout << "Stack length: " << top << "; ";
-        std::cout << "Stack: [";
-
+        std::string stack_content;
         for (int i = 1; i <= top; i++) {
             const int t = lua_type(Lua, i);
 
             switch (t) {
                 case LUA_TSTRING:
-                    std::cout << lua_tostring(Lua, i);
+                    stack_content += lua_tostring(Lua, i);
                     break;
                 case LUA_TBOOLEAN:
-                    std::cout << (lua_toboolean(Lua, i) ? "true" : "false");
+                    stack_content += (lua_toboolean(Lua, i) ? "true" : "false");
                     break;
                 case LUA_TNUMBER:
-                    std::cout << lua_tonumber(Lua, i);
+                    stack_content += std::to_string(lua_tonumber(Lua, i));
                     break;
                 default:
-                    std::cout << lua_typename(Lua, t);
+                    stack_content += lua_typename(Lua, t);
                     break;
             }
-            std::cout << ", ";
+            stack_content += ", ";
         }
-        std::cout << "]; " << std::endl;
+
+        absp_logger_console->debug("Stack length: {}; Stack: [{}]", top, stack_content);
     }
 
     void update_digitizers(std::vector<ABCD::Digitizer*> &digitizers) {
@@ -152,20 +157,14 @@ public:
         lua_pushinteger(Lua, counter_updates);
         lua_setglobal(Lua, "counter_updates");
 
-        if (verbosity > 0) {
-            std::cout << "LuaManager::update_digitizers() ";
-            std::cout << "Number of digitizers: " << digitizers.size() << "; ";
-            std::cout << std::endl;
-        }
+        absp_logger_console->info("LuaManager::update_digitizers() Number of updates: {}", counter_updates);
+
+        absp_logger_console->info("LuaManager::update_digitizers() Number of digitizers: {}", digitizers.size());
 
         lua_getglobal(Lua, "digitizer_instances");
 
         for (auto &digitizer: digitizers) {
-            if (verbosity > 0) {
-                std::cout << "LuaManager::update_digitizers() ";
-                std::cout << "Found digitizer: " << digitizer->GetModel() << "; ";
-                std::cout << std::endl;
-            }
+            absp_logger_console->info("LuaManager::update_digitizers() Found digitizer: {}", digitizer->GetModel());
 
             if (digitizer->GetModel() == "ADQ214") {
                 ABCD::ADQ214 *digitizer_adq = reinterpret_cast<ABCD::ADQ214*>(digitizer);
@@ -204,11 +203,7 @@ public:
                 lua_setfield(Lua, -2, digitizer->GetName().c_str());
 
             } else {
-                if (verbosity > 0) {
-                    std::cout << "LuaManager::update_digitizers() ";
-                    std::cout << WRITE_YELLOW << "WARNING:" << WRITE_NC << " Using the generic Digitizer interface for the unexpected digitizer: " << digitizer->GetModel() << " name: " << digitizer->GetName() << "; ";
-                    std::cout << std::endl;
-                }
+                absp_logger_console->warn("LuaManager::update_digitizers() Using the generic Digitizer interface for the unexpected digitizer: {}; name: {}", digitizer->GetModel(), digitizer->GetName());
 
                 SWIG_NewPointerObj(Lua, digitizer, SWIG_ABCD_Digitizer_t, 0);
 
@@ -220,8 +215,9 @@ public:
         // otherwise it would pile-up in it multiple times.
         lua_pop(Lua, 1);
 
-        if (verbosity > 0) {
-            run_string("print(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::update_digitizers() Finished\");");
+        if (absp_logger_console->should_log(spdlog::level::debug))
+        {
+            run_string("abcd_log(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::update_digitizers() Finished\");");
 
             dump_stack();
         }
@@ -233,36 +229,35 @@ public:
         lua_pushinteger(Lua, counter_runs);
         lua_setglobal(Lua, "counter_runs");
 
-        if (verbosity > 0) {
-            std::cout << "LuaManager::run_string() ";
-            std::cout << "Running string; ";
-            std::cout << std::endl;
-        }
+        absp_logger_console->info("LuaManager::run_string() Number of runs: {}", counter_runs);
 
         const int result = luaL_dostring(Lua, source.c_str());
 
         if (result != LUA_OK) { // there was an error
+            // Get the error message from the Lua stack
+            const std::string error_message = lua_tostring(Lua, -1);
+
             switch (result) {
                 case LUA_ERRSYNTAX:
-                    std::cout << WRITE_RED << "LUA ERROR" << WRITE_NC << ": Syntax error: ";
+                    absp_logger_error->error("LuaManager::run_string() Syntax error: {}", error_message);
                     break;
-                case LUA_ERRMEM:                                                                                             std::cout << WRITE_RED << "LUA ERROR" << WRITE_NC << ": Memory allocation error: ";
+                case LUA_ERRMEM:
+                    absp_logger_error->error("LuaManager::run_string() Memory allocation error: {}", error_message);
                     break;
                 case LUA_ERRFILE:
-                    std::cout << WRITE_RED << "LUA ERROR" << WRITE_NC << ": File error: ";
+                    absp_logger_error->error("LuaManager::run_string() File error: {}", error_message);
                     break;
                 case LUA_ERRRUN:
-                    std::cout << WRITE_RED << "LUA ERROR" << WRITE_NC << ": Runtime error: ";
+                    absp_logger_error->error("LuaManager::run_string() Runtime error: {}", error_message);
                     break;
-                case LUA_ERRERR:                                                                                             std::cout << WRITE_RED << "LUA ERROR" << WRITE_NC << ": Error handler error: ";
+                case LUA_ERRERR:
+                    absp_logger_error->error("LuaManager::run_string() Error handler error: {}", error_message);
                     break;
                 default:
-                    std::cout << WRITE_RED << "LUA ERROR" << WRITE_NC << ": Unknown error: ";
+                    absp_logger_error->error("LuaManager::run_string() Unknown error: {}", error_message);
                     break;
             }
 
-            // Get the error message from the Lua stack and print it
-            std::cout << lua_tostring(Lua, -1) << std::endl;
             // Pop the error message from the Lua stack
             lua_pop(Lua, 1);
         }
@@ -273,13 +268,7 @@ public:
                     std::string when,
                     std::string source)
     {
-        if (verbosity > 0) {
-            std::cout << "LuaManager::run_script() ";
-            std::cout << "Running script; ";
-            std::cout << "Current state ID: " << state_ID << "; ";
-            std::cout << "Current state description: " << state_description << "; ";
-            std::cout << std::endl;
-        }
+        absp_logger_console->info("LuaManager::run_script() Running script; Current state ID: {}; Current state description: {}; When: {}", state_ID, state_description, when);
 
         lua_getglobal(Lua, "current_state");
 
@@ -296,11 +285,43 @@ public:
 
         run_string(source);
 
-        if (verbosity > 0) {
-            run_string("print(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::run_script() Finished\");");
+        if (absp_logger_console->should_log(spdlog::level::debug))
+        {
+            run_string("abcd_log(\"LUA INTERPRETER DEBUG OUTPUT: LuaManager::run_script() Finished\");");
 
             dump_stack();
         }
+    }
+
+    inline static int abcd_log(lua_State *L) {
+        // Get the number of arguments
+        const int n = lua_gettop(L);
+        std::string output;
+
+        for (int index = 1; index <= n; index += 1) {
+            if (lua_isstring(L, index))
+            {
+                const char* argument = lua_tostring(L, index);
+
+                output += argument;
+                output += " ";
+
+            } else if (lua_isnumber(L, index)) {
+                const lua_Number argument = lua_tonumber(L, index);
+
+                output += std::to_string(argument);
+                output += " ";
+            } else {
+                lua_pushliteral(L, "the argument of the abcd_log() function must be a string or a number");
+                lua_error(L);
+
+                return 0;
+            }
+        }
+
+        absp_logger_console->info(output);
+
+        return 0;
     }
 
     inline static int sleep(lua_State *L) {
